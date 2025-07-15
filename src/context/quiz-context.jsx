c8c5ae13b217788129248f9f679f240dcdfc7d99
Sharan Shah <shahshar@SYNOVERGETECH.COM>
2025-07-15 15:14:31 +0530

    Initial commit

---
+++ src/context/quiz-context.jsx
@@ -0,0 +1,4 @@
+// src/context/quiz-context.js
+import { createContext } from 'react';
+
+export const QuizContext = createContext();
---
+++ src/context/quiz-provider.jsx
@@ -0,0 +1,72 @@
+// src/context/quiz-provider.jsx
+import React, { useState } from 'react';
+import { QuizContext } from './quiz-context';
+
+const QuizProvider = ({ children }) => {
+  const quizData = [
+    {
+    question: "What is the primary purpose of React?",
+    options: [
+      "To style web applications",
+      "To build user interfaces",
+      "To manage databases",
+      "To handle server-side logic"
+    ],
+    answer: "To build user interfaces",
+  },
+  {
+    question: "Which hook is used to manage state in a functional React component?",
+    options: [
+      "useRef",
+      "useEffect",
+      "useState",
+      "useContext"
+    ],
+    answer: "useState",
+  },
+  {
+    question: "What is JSX?",
+    options: [
+      "A server-side language",
+      "A CSS preprocessor",
+      "A syntax extension for JavaScript",
+      "A database query language"
+    ],
+    answer: "A syntax extension for JavaScript",
+  },
+  ];
+
+  const initialAnswers = quizData.map(() => null);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState(initialAnswers);
+
+  const setAnswerForQuestion = (index, answer) => {
+    setUserAnswers((prevAnswers) => {
+      const newAnswers = [...prevAnswers];
+      newAnswers[index] = answer;
+      return newAnswers;
+    });
+  };
+
+  const resetQuiz = () => {
+    setCurrentQuestion(0);
+    setUserAnswers(quizData.map(() => null));
+  };
+
+  return (
+    <QuizContext.Provider
+      value={{
+        quizData,
+        currentQuestion,
+        setCurrentQuestion,
+        userAnswers,
+        setAnswerForQuestion,
+        resetQuiz,
+      }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
+};
+
+export default QuizProvider;
